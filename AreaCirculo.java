13bc84bbe9b3e0e390daf01bf4f9d78caa5ae635
robfisico <santosarobson@gmail.com>
2022-05-26 01:18:45 -0300

    first commit

---
+++ AreaCirculo.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class AreaCirculo {
+
+	public static void main(String[] args) {
+		//System.out.println(2+3);
+		double raio = 3.4;
+		final double pi = 3.14159;
+		
+		double area = pi * raio * raio;
+		System.out.println("O valor da area de um circulo de raio " + raio + " e " + area + " m2");
+	}
+}
