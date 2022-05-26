e7c77d58638d13402af6f21e6394ce1ad39f161d
robfisico <santosarobson@gmail.com>
2022-05-26 01:38:19 -0300

    second commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,29 @@
+package fundamentos;
+
+/**
+ * Essa classe representa... JAVADOC
+ * @author robso
+ * @since JDK1.0
+ * @see
+ */
+
+public class PrimeiroPrograma {
+	
+	/**
+	 * 
+	 * @param args
+	 * @  
+	 */
+
+	//Uma sentença de código termina com ;
+	public static void main(String[] args) {
+		System.out.println("Primeiro programa Parte #01!");
+		/*
+		 Linha 1
+		 Linha 2
+		 Linha 3
+		 */
+		System.out.println("Primeiro programa Parte #02!");
+
+	}
+}
